64625bc5ff693bb726bc3651ff1feab15c79032d
Emil88020 <kkonarzewski04@gmail.com>
2024-12-03 14:55:59 +0100

    Second project from work

---
+++ Excel_to_Excel/ModelUpdater/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ModelUpdater
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
